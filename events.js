8d73d419c36f710c5b79ac29d24decaed4f84012
projectxr <prashant95c@gmail.com>
2019-10-14 21:14:40 +0530

    Add files via upload

--- events.js
+++ events.js
@@ -1,11 +1,11 @@
 AFRAME.registerComponent('markerhandler', {
 
     init: function() {
-        const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector("#animated-model");
+        const animatedMarker = document.querySelector("#parkingMarker");
+        const aEntity = document.querySelector("#parkingHero");
 
         animatedMarker.addEventListener('click', function(ev, target){
-            
+            console.log("click Detected");
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
             if (aEntity && intersectedElement === aEntity) {
                 console.log("click Detected");
